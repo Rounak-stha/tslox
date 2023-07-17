5fc6a82d40296f19853e01d8d4743cbed8690d06
Rouak-stha <rounakshrestha1153@gmail.com>
2023-07-17 23:55:11 +0545

    Aded Expression Tests

---
+++ test/expression.test.ts
@@ -0,0 +1,33 @@
+import { Binary, Unary, Grouping, Literal, Expr } from '../src/expression'
+import Token from '../src/tokenizer/Token'
+import TokenType from '../src/tokenizer/TokenType'
+
+describe('All Expression Class Test', () => {
+	it('Test Binary Expression', () => {
+		const multiplicationExpression = new Binary(2, new Token(TokenType.PLUS, '*', '', 1), 2)
+		const aditionExpresion = new Binary(1, new Token(TokenType.PLUS, '+', '', 1), multiplicationExpression)
+		const subtractionExpression = new Binary(aditionExpresion, new Token(TokenType.MINUS, '-', '', 1), 9)
+
+		expect(multiplicationExpression.left).toBe(2)
+		expect(multiplicationExpression.right).toBe(2)
+		expect(multiplicationExpression.operator.lexeme).toBe('*')
+
+		expect(aditionExpresion.left).toBe(1)
+		expect(aditionExpresion.right).toBe(multiplicationExpression)
+
+		expect(subtractionExpression.left).toBe(aditionExpresion)
+	})
+
+	it('Test Unary Expression', () => {
+		const numberLiteral1 = new Literal(1)
+		const booleanLiteralFalse = new Literal(false)
+		const negativeUnary = new Unary(new Token(TokenType.MINUS, '-', '', 1), numberLiteral1)
+		const negationUnary = new Unary(new Token(TokenType.BANG, '!', '', 1), booleanLiteralFalse)
+
+		expect(negativeUnary.operator).toEqual(new Token(TokenType.MINUS, '-', '', 1))
+		expect(negativeUnary.right).toBe(numberLiteral1)
+
+		expect(negationUnary.operator).toEqual(new Token(TokenType.BANG, '!', '', 1))
+		expect(negationUnary.right).toBe(booleanLiteralFalse)
+	})
+})
