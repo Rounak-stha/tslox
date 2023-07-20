ade1a9fb9bc78b11e978d7504ff740749b5d1836
Rouak-stha <rounakshrestha1153@gmail.com>
2023-07-20 19:08:09 +0545

    Changed Error Message

--- src/error/LoxError.ts
+++ src/error/LoxError.ts
@@ -7,22 +7,22 @@ class LoxError extends Error {
 	constructor(line: number, type: ErrorType, message?: string) {
 		switch (type) {
 			case 'Unknown Lexeme':
-				super(`Unknown Lexeme at line: ${line}${message ? '\n' + message : ''}`)
+				super(`[Unknown Lexeme | Line: ${line}]${message ? ' ' + message : ''}`)
 				this.code = 401
 				this.type = type
 				break
 			case 'Syntax':
-				super(`Syntax Error at line: ${line}${message ? '\n' + message : ''}`)
+				super(`[Syntax Error | Line: ${line}]${message ? ' ' + message : ''}`)
 				this.code = 402
 				this.type = type
 				break
 			case 'Unterminated String':
-				super(`Unterminated String at line: ${line}${message ? '\n' + message : ''}`)
+				super(`[Unterminated String | Line: ${line}]${message ? ' ' + message : ''}`)
 				this.code = 403
 				this.type = type
 				break
 			case 'Runtime':
-				super(`Runtime Error at line: ${line}${message ? '\n' + message : ''}`)
+				super(`[Runtime Error | Line: ${line}]${message ? ' ' + message : ''}`)
 				this.code = 403
 				this.type = type
 				break
