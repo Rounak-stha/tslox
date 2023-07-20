a1e34ed6254aa32b822349cad94ff78e05548bd9
Rouak-stha <rounakshrestha1153@gmail.com>
2023-07-20 19:08:29 +0545

    Run File or Prompt

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,55 @@
+import fs from 'fs'
+import path from 'path'
 import { Expr } from './expression'
-const readline = require('readline').createInterface({
-	input: process.stdin,
-	output: process.stdout
-})
-
 import Parser from './parser'
 import Tokenizer from './tokenizer/tokenizer'
 import AstToString from './AstToString'
 import { Interpreter } from './interpreter'
 
-/* const tokenizer = new Tokenizer('1+2 - (7 * 8 - 9)')
-tokenizer.scanTokens()
-const parser = new Parser(tokenizer.Tokens)
-const result = parser.parse()
-console.log(JSON.stringify(result, null, 4))
+// const astPrinter = new AstToString()
+// console.log(astPrinter.print(result))
 
-if (result) {
-	const astPrinter = new AstToString()
-	console.log(astPrinter.print(result))
-} */
-
-async function prompt(): Promise<string> {
-	return new Promise((resolve) => {
-		readline.question('>', (source: string) => resolve(source))
-	})
+function run(source: string) {
+	const interpreter = new Interpreter()
+	const tokenizer = new Tokenizer(source)
+	tokenizer.scanTokens()
+	const parser = new Parser(tokenizer.Tokens)
+	const expr = parser.parse()
+	console.log(JSON.stringify(expr, null, 4))
+	interpreter.interpret(expr as Expr)
 }
+
+function runFile(filePath: string) {
+	if (!path.isAbsolute(filePath)) filePath = path.join(process.cwd(), filePath)
+	const source = fs.readFileSync(filePath, { encoding: 'utf-8' })
+	run(source)
+}
+
+async function runPrompt() {
+	const readline = require('readline').createInterface({
+		input: process.stdin,
+		output: process.stdout
+	})
+	async function prompt(): Promise<string> {
+		return new Promise((resolve) => {
+			readline.question('>', (source: string) => resolve(source))
+		})
+	}
+
+	for (;;) {
+		const source = await prompt()
+		run(source)
+	}
+}
+
+;(async function main() {
+	const filePath = process.argv[2]
+	if (filePath) {
+		runFile(filePath)
+	} else {
+		runPrompt()
+	}
+})()
 
 export function createAst(source: string) {
 	const tokenizer = new Tokenizer(source)
@@ -35,16 +60,3 @@ export function createAst(source: string) {
 		return JSON.stringify(result, null, 4)
 	} else console.log('An Error Occoured')
 }
-
-;(async function () {
-	const interpreter = new Interpreter()
-	for (;;) {
-		const source = await prompt()
-		const tokenizer = new Tokenizer(source)
-		tokenizer.scanTokens()
-		const parser = new Parser(tokenizer.Tokens)
-		const expr = parser.parse()
-
-		interpreter.interpret(expr as Expr)
-	}
-})()
