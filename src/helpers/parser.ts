389806d7351cabb1a6a74daf2a73560f0d9124a1
Rouak-stha <rounakshrestha1153@gmail.com>
2023-07-20 19:52:54 +0545

    Added Visualizer Helpers

---
+++ src/helpers/parser.ts
@@ -0,0 +1,44 @@
+import path from 'path'
+import fs from 'fs'
+import Tokenizer from '../tokenizer/tokenizer'
+import Parser from '../parser'
+
+function run(source: string) {
+    const tokenizer = new Tokenizer(source)
+    tokenizer.scanTokens()
+    const parser = new Parser(tokenizer.Tokens)
+    const expr = parser.parse()
+    console.log(expr)
+}
+
+function runFile(filePath: string) {
+    if (!path.isAbsolute(filePath)) filePath = path.join(process.cwd(), filePath)
+    const source = fs.readFileSync(filePath, { encoding: 'utf-8' })
+    run(source)
+}
+
+async function runPrompt() {
+    const readline = require('readline').createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    })
+    async function prompt(): Promise<string> {
+        return new Promise((resolve) => {
+            readline.question('>', (source: string) => resolve(source))
+        })
+    }
+
+    for (;;) {
+        const source = await prompt()
+        run(source)
+    }
+}
+
+;(async function main() {
+    const filePath = process.argv[2]
+    if (filePath) {
+        runFile(filePath)
+    } else {
+        runPrompt()
+    }
+})()
---
+++ src/helpers/token.ts
@@ -0,0 +1,41 @@
+import path from 'path'
+import fs from 'fs'
+import Tokenizer from '../tokenizer/tokenizer'
+
+function run(source: string) {
+    const tokenizer = new Tokenizer(source)
+    tokenizer.scanTokens()
+    console.log(tokenizer.Tokens)
+}
+
+function runFile(filePath: string) {
+    if (!path.isAbsolute(filePath)) filePath = path.join(process.cwd(), filePath)
+    const source = fs.readFileSync(filePath, { encoding: 'utf-8' })
+    run(source)
+}
+
+async function runPrompt() {
+    const readline = require('readline').createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    })
+    async function prompt(): Promise<string> {
+        return new Promise((resolve) => {
+            readline.question('>', (source: string) => resolve(source))
+        })
+    }
+
+    for (;;) {
+        const source = await prompt()
+        run(source)
+    }
+}
+
+;(async function main() {
+    const filePath = process.argv[2]
+    if (filePath) {
+        runFile(filePath)
+    } else {
+        runPrompt()
+    }
+})()
