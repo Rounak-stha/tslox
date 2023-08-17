0cdbd83edf7bece0b838ce9007f1cc76bdb73b07
Rounak Shrestha <rounak.shrestha@compliancequest.com>
2023-08-17 22:43:10 +0545

    Added Footer

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -2,18 +2,19 @@ import Editor from './components/Editor'
 import OutputContainer from './components/OutputContainer'
 import { Provider } from 'react-redux'
 import { store } from './store'
+import Footer from './components/Footer'
 
 function App() {
     return (
         <main className="flex flex-col h-screen max-h-screen">
-            <div className="h-6">Header</div>
+            <div className="h-6 font-semibold">TSLox AST Explorer</div>
             <div className="flex flex-1 min-h-0">
                 <Provider store={store}>
                     <Editor />
                     <OutputContainer />
                 </Provider>
             </div>
-            <div className="h-6">Footer</div>
+            <Footer />
         </main>
     )
 }
---
+++ website/src/components/Footer.tsx
@@ -0,0 +1,30 @@
+import { Feedback, Github } from '../icons'
+
+export default function Footer() {
+    return (
+        <div className="h-6 text-xs mono flex items-center justify-center text-blue-600">
+            <a href="https://github.com/rounak-stha/tslox/issues">
+                <span className="flex mr-4 hover:underline">
+                    <span className="h-[14px] w-[14px] mr-1 fill-blue-600 hover:underline">
+                        <Feedback />
+                    </span>
+                    Feedback
+                </span>
+            </a>
+            <a href="https://github.com/rounak-stha/tslox">
+                <span className="mr-4 flex hover:underline">
+                    <span className="mr-1">
+                        <Github />
+                    </span>
+                    Github
+                </span>
+            </a>
+            <a href="https://www.rounakstha.me">
+                <span className="mr-4 flex hover:underline">Developer</span>
+            </a>
+            <a href="https://craftinginterpreters.com">
+                <span className="mr-4 flex hover:underline">Language</span>
+            </a>
+        </div>
+    )
+}
