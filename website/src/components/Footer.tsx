68e3af7fcb68f293bc1143af54504864ff5a0c5c
Rounak Shrestha <rounak.shrestha@compliancequest.com>
2023-08-18 22:22:16 +0545

    Open footer links in new tab

--- website/src/components/Footer.tsx
+++ website/src/components/Footer.tsx
@@ -2,8 +2,8 @@ import { Feedback, Github } from '../icons'
 
 export default function Footer() {
     return (
-        <div className="h-6 text-xs mono flex items-center justify-center text-blue-600">
-            <a href="https://github.com/rounak-stha/tslox/issues">
+        <footer className="h-6 text-xs mono flex items-center justify-center text-blue-600">
+            <a href="https://github.com/Rounak-stha/tslox/issues/new" target="_blank" >
                 <span className="flex mr-4 hover:underline">
                     <span className="h-[14px] w-[14px] mr-1 fill-blue-600 hover:underline">
                         <Feedback />
@@ -11,7 +11,7 @@ export default function Footer() {
                     Feedback
                 </span>
             </a>
-            <a href="https://github.com/rounak-stha/tslox">
+            <a href="https://github.com/rounak-stha/tslox" target="_blank" >
                 <span className="mr-4 flex hover:underline">
                     <span className="mr-1">
                         <Github />
@@ -19,12 +19,12 @@ export default function Footer() {
                     Github
                 </span>
             </a>
-            <a href="https://www.rounakstha.me">
+            <a href="https://www.rounakstha.me" target="_blank" >
                 <span className="mr-4 flex hover:underline">Developer</span>
             </a>
-            <a href="https://craftinginterpreters.com">
+            <a href="https://craftinginterpreters.com" target="_blank" >
                 <span className="mr-4 flex hover:underline">Language</span>
             </a>
-        </div>
+        </footer>
     )
 }
