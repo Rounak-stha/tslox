3bea5630392a55a2817e6e87ff36ea33a69f7c07
Rouak-stha <rounakshrestha1153@gmail.com>
2023-08-19 16:47:05 +0545

    added vercel analytics

--- website/src/main.tsx
+++ website/src/main.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { inject } from '@vercel/analytics'
 import './index.css'
+
+inject()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
