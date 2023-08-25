96d8cd49367644214840bc37fcfa0619ca47682f
Rounak Shrestha <rounak.shrestha@compliancequest.com>
2023-08-25 16:44:38 +0545

    Created and Used NodeHelper Class

--- website/src/components/visualizations/Block.tsx
+++ website/src/components/visualizations/Block.tsx
@@ -1,45 +1,16 @@
 import { useState } from 'react'
 import { Node } from '../../../../tox/src/types'
-import { isNullOrUndefined } from '../../utils/helpers'
-import { publish } from '../../utils/pubSub'
+import { NodeHelper } from '../../utils/vUtil'
+
+const nodeHelper = new NodeHelper()
 
 export default function Block({ name, node, children }: { name: string; children: React.ReactNode; node: Node | Node[] }) {
     const [showChildren, setShowChildren] = useState(true)
-    const isArray = Array.isArray(node)
-    let startChar: string
-    let endChar: string
-    let from: number
-    let to: number
+    const { startChar, endChar } = nodeHelper.getstartAndEndLiterals(node)
+    const propStr = nodeHelper.getPropStr(node)
 
-    if (isArray) {
-        startChar = '['
-        endChar = ']'
-        if (node.length) {
-            from = node[0].from
-            to = node[node.length - 1].to
-        }
-    } else {
-        startChar = '{'
-        endChar = '}'
-        from = node.from
-        to = node.to
-    }
-
-    const propStr = isArray ? node.length.toString() + ' element' + (node.length > 1 ? 's' : '') : Object.keys(node).join(', ')
-
-    const handleMouseOver = (e: React.MouseEvent<HTMLLIElement>) => {
-        e.stopPropagation()
-        ;(e.target as HTMLUListElement).classList.add('node-hover')
-        !isNullOrUndefined(from) && !isNullOrUndefined(to) && publish('HIGHLIGHT', { from, to })
-    }
-
-    const handeMouseOut = (e: React.MouseEvent<HTMLLIElement>) => {
-        e.stopPropagation()
-        ;(e.target as HTMLUListElement).classList.remove('node-hover')
-        // Remove the highlight
-    }
     return (
-        <li className="py-1 list-none" onMouseOver={handleMouseOver} onMouseOut={handeMouseOut}>
+        <li className="py-1 list-none" onMouseOver={nodeHelper.getMouseOverHandler(node)} onMouseOut={nodeHelper.getMouseOuthandler()}>
             <span onClick={() => setShowChildren((prev) => !prev)}>
                 <span className="pr-1.5">{showChildren ? '-' : '+'}</span>
                 <span className="text-blue-500 hover:underline cursor-pointer">{name}</span>
