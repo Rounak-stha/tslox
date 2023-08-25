7017785faed13064bf71676713c381bcf6468e8f
Rounak Shrestha <rounak.shrestha@compliancequest.com>
2023-08-25 11:55:53 +0545

    Component Key Refactor

--- website/src/components/visualizations/Element.tsx
+++ website/src/components/visualizations/Element.tsx
@@ -5,13 +5,14 @@ import VPrimary from './VPrimary'
 export default function Element({ node }: { node: Node | Node[] }) {
     if (Array.isArray(node)) return node.map((n) => <Element key={n.type + n.from.toString() + n.to.toString()} node={n} />)
     const elems = []
-    for (const key in node) {
-        const a: unknown = node[key as keyof Node]
+    for (const prop in node) {
+        const key = node.type + prop + node.from.toString()
+        const a: unknown = node[prop as keyof Node]
         if (Array.isArray(a)) {
             elems.push(
-                <Block name={key} node={a}>
+                <Block key={key} name={prop} node={a}>
                     {(a as Node[]).map((n) => (
-                        <Block key={key + n.type + n.from.toString() + n.to.toString()} name={n.type} node={n}>
+                        <Block key={prop + n.type + n.from.toString() + n.to.toString()} name={n.type} node={n}>
                             <Element node={n} />
                         </Block>
                     ))}
@@ -21,13 +22,13 @@ export default function Element({ node }: { node: Node | Node[] }) {
         }
         if (a && typeof a === 'object') {
             elems.push(
-                <Block name={key} node={a as Node}>
+                <Block key={key} name={prop} node={a as Node}>
                     <Element node={a as Node} />
                 </Block>
             )
             continue
         }
-        elems.push(<VPrimary name={key} value={a as string | number} />)
+        elems.push(<VPrimary key={key} name={prop} value={a as string | number} />)
     }
     return elems
 }
